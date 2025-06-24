cc3780a9db2e785dc43cc0cbe528a088ec52ea69
Abhay Chaudhary <abhaycdry10@gmail.com>
2025-06-24 14:34:21 +0545

    list course

--- src/app/pages/admin-component/list-course/list-course.component.ts
+++ src/app/pages/admin-component/list-course/list-course.component.ts
@@ -5,7 +5,6 @@ import { CommonModule } from '@angular/common';
 import * as alertify from 'alertifyjs';
 import { PopUpService } from '../../../core/popup/pop-up.service';
 
-
 @Component({
   selector: 'app-list-course',
   standalone: true,
@@ -14,56 +13,92 @@ import { PopUpService } from '../../../core/popup/pop-up.service';
   styleUrl: './list-course.component.css'
 })
 export class ListCourseComponent {
-  listData: any[] = []
-  constructor(private http: HttpClient, private courseListService: EnrollmentService,
-    private confirmationService:PopUpService
+  listData: any[] = [];
 
+  constructor(
+    private http: HttpClient,
+    private courseListService: EnrollmentService,
+    private confirmationService: PopUpService
   ) {
     this.getEnrollmentList();
   }
+
   getEnrollmentList() {
     this.courseListService.getEnrollmentData().subscribe((res) => {
       console.log(res);
-      this.listData = res
+      this.listData = res;
       console.log(this.listData);
-      debugger
-    })
+      debugger;
+    });
   }
-  async deleteEnrollData(enrollId: string) {
 
-    const confirmed = this.showConfirmationPopup();
-    if (await confirmed) {
+  async deleteEnrollData(enrollId: string) {
+    const confirmed = await this.showConfirmationPopup();
+    if (confirmed) {
       this.courseListService.delEnrollmentList(enrollId).subscribe((res) => {
         console.log(res);
         this.getEnrollmentList();
+        this.confirmationService.showSuccessMessage('Enrollment deleted successfully');
+      }, () => {
+        this.confirmationService.showErrorMessage('Failed to delete enrollment');
       });
     }
   }
+
   showConfirmationPopup(): Promise<boolean> {
     return new Promise((resolve) => {
-      alertify.confirm('Confirmation', 'Are you sure you want to delete this enrollment?', () => {
-        resolve(true);
-      }, () => {
-        resolve(false);
-      });
+      alertify.confirm(
+        'Confirmation',
+        'Are you sure you want to delete this enrollment?',
+        () => {
+          resolve(true);
+        },
+        () => {
+          resolve(false);
+        }
+      );
     });
   }
+
   async deleteSubject(enrollmentId: string, subjectCode: string): Promise<void> {
-    const confirmed = this.showConfirmationPopup();
-    if (await confirmed) {
-
-      this.courseListService.deleteSubjectFromEnrollment(enrollmentId, subjectCode).subscribe((res) => {
-        console.log(res);
-        this.getEnrollmentList();
-        this.confirmationService.showSuccessMessage('Delete Sucessfully')
-
-      })
+    const confirmed = await this.showConfirmationPopup();
+    if (!confirmed) {
+      this.confirmationService.showErrorMessage('Sorry, cannot be deleted');
+      return;
     }
-    else{
-      this.confirmationService.showErrorMessage('Sorry Cannnot be Deleted')
+
+    // Find the enrollment in the listData
+    const enrollment = this.listData.find(enroll => enroll._id === enrollmentId);
+
+    if (!enrollment) {
+      this.confirmationService.showErrorMessage('Enrollment not found');
+      return;
+    }
+
+    const subjectCount = enrollment.subjects.length;
+
+    if (subjectCount === 1) {
+      // Only one subject, delete the whole enrollment
+      this.courseListService.delEnrollmentList(enrollmentId).subscribe(
+        () => {
+          this.getEnrollmentList();
+          this.confirmationService.showSuccessMessage('Enrollment deleted successfully');
+        },
+        () => {
+          this.confirmationService.showErrorMessage('Failed to delete enrollment');
+        }
+      );
+    } else {
+      // More than one subject, delete only the subject
+      this.courseListService.deleteSubjectFromEnrollment(enrollmentId, subjectCode).subscribe(
+        () => {
+          this.getEnrollmentList();
+          this.confirmationService.showSuccessMessage('Subject deleted successfully');
+        },
+        () => {
+          this.confirmationService.showErrorMessage('Failed to delete subject');
+        }
+      );
     }
   }
-
-
-
 }
